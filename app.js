cbda7c00cc06af212656f30d0911524ed026987b
SanjayJatti <jattisanjay.r@gmail.com>
2021-09-04 20:14:41 -0700

    commit

--- app.js
+++ app.js
@@ -1,40 +1,45 @@
 const billAmount=document.querySelector("#bill-amount");
 const cashGiven=document.querySelector("#cash-given");
+const cashGivenDiv=document.querySelector(".cash-given-div")
 const nextButton=document.querySelector("#next-button");
 const errorMessage=document.querySelector(".error-message");
 const checkButton=document.querySelector("#check-button");
-let noOfNotes=document.querySelector(".noOfNotes");
+const noOfNotes=document.querySelectorAll(".noOfNotes");
 const changeReturn=document.querySelector(".change-return");
 
 const notes = [2000,500,200,100,50,20,10,5,1];
 
-nextButton.addEventListener('click',()=>{
+nextButton.addEventListener("click",()=>{
     hideError();
     if(billAmount.value>0){
         nextButton.style.display="none";
-        cashGiven.style.display="block";
+        cashGivenDiv.style.display="block";
     }
     else{
         showError("Enter valid bill amount");
     }
 } )
 
-checkButton.addEventListener('click',()=>{
-if (cashGiven.value>=billAmount.value){
-    const returnAmount= cashGiven.value - billAmount.value;
+checkButton.addEventListener("click",()=>{
+console.log(noOfNotes);
+console.log(parseInt(cashGiven.value)>=parseInt(billAmount.value));
+
+if (parseInt(cashGiven.value)>=parseInt(billAmount.value)){
+    changeReturn.style.display="block";
+    const returnAmount= parseInt(cashGiven.value) - parseInt(billAmount.value);
     calculation(returnAmount);
+    
 }else{
     showError("Cash given is less than bill amount.Please enter right amount.");
 
 } 
-})
+});
 
-function calulation(returnAmount){
+function calculation(returnAmount){
 for(let i=0; i<notes.length; i++){
     const numberOfNotes= Math.trunc(returnAmount/notes[i]);
     returnAmount %= notes[i];
      noOfNotes[i].innerText=numberOfNotes;
-    
 }
 }
 function hideError(){
@@ -44,5 +49,4 @@ function showError(msg){
     errorMessage.style.display="block";
     errorMessage.innerText=msg;
     changeReturn.style.display="none"; 
-
 }
